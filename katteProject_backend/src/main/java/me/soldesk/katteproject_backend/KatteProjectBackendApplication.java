0b3562dae677cc5e1219314698d14c13fc4abcc2
minquu <minquu@gmail.com>
2025-05-21 18:12:51 +0900

    백엔드 프로젝트 기본적인 스프링 부트 프로젝트 업로드

---
+++ katteProject_backend/src/main/java/me/soldesk/katteproject_backend/KatteProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package me.soldesk.katteproject_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KatteProjectBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KatteProjectBackendApplication.class, args);
+    }
+
+}
---
+++ katteProject_backend/src/test/java/me/soldesk/katteproject_backend/KatteProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package me.soldesk.katteproject_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KatteProjectBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
