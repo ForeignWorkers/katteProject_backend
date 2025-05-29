e0011c8589bd75fd051ae58a1b57a55e871383d9
minquu <minquu@gmail.com>
2025-05-29 17:12:58 +0900

    옥션 입찰 등록 기능 추가

--- katteProject_backend/src/main/java/me/soldesk/katteproject_backend/mapper/AuctionMapper.java
+++ katteProject_backend/src/main/java/me/soldesk/katteproject_backend/mapper/AuctionMapper.java
@@ -1,9 +1,10 @@
 package me.soldesk.katteproject_backend.mapper;
 
+import common.bean.auction.AuctionBidLog;
 import me.soldesk.katteproject_backend.test.ProductPriceRegisterBean;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
 
 @Mapper
 public interface AuctionMapper {
@@ -31,4 +32,20 @@ public interface AuctionMapper {
     """)
     @Options(useGeneratedKeys = true, keyProperty = "id")
     void insertAuctionData(ProductPriceRegisterBean productPriceRegisterBean);
+
+    @Insert("INSERT INTO auction_bid_log(user_id, auction_data_id, bid_take_time, bid_price, bid_limit_time)\n" +
+            "VALUES (#{user_id},#{auction_data_id},NOW(),#{bid_price},#{bid_limit_time});")
+    void addAcutionBidLog(AuctionBidLog auctionBidLog);
+
+    @Select("SELECT * FROM auction_bid_log WHERE user_id = #{user_id}")
+    List<AuctionBidLog> getAuctionBidLogByUserId(int userId);
+
+    @Select("SELECT * FROM auction_bid_log WHERE auction_data_id = #{auction_id}")
+    List<AuctionBidLog> getAuctionBidLogByAuctionId(int auction_id);
+
+    @Select("SELECT * FROM auction_bid_log WHERE id = #{bid_id}")
+    List<AuctionBidLog>  getAuctionBidLogByBidId(int bid_id);
+
+    @Delete("DELETE FROM auction_bid_log WHERE id = #{bid_id}")
+    void deleteAuctionLogByBidId(int bid_id);
 }
--- katteProject_backend/src/main/java/me/soldesk/katteproject_backend/service/AuctionService.java
+++ katteProject_backend/src/main/java/me/soldesk/katteproject_backend/service/AuctionService.java
@@ -1,10 +1,14 @@
 package me.soldesk.katteproject_backend.service;
+import common.bean.auction.AuctionBidLog;
+import common.bean.user.UserBean;
 import lombok.RequiredArgsConstructor;
 
 import me.soldesk.katteproject_backend.mapper.AuctionMapper;
 import me.soldesk.katteproject_backend.test.ProductPriceRegisterBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AuctionService {
@@ -14,5 +18,25 @@ public class AuctionService {
 
     public void registerAuction(ProductPriceRegisterBean productPriceRegisterBean) {
         auctionMapper.insertAuctionData(productPriceRegisterBean);
+    }
+
+    public void addAuctionBidLog(AuctionBidLog auctionBidLog) {
+        auctionMapper.addAcutionBidLog(auctionBidLog);
+    }
+
+    public List<AuctionBidLog> getAuctionBidLog(String user_id, String bid_id) {
+        if(user_id != null && !user_id.trim().isEmpty()) {
+            return auctionMapper.getAuctionBidLogByUserId(Integer.parseInt(user_id));
+        }else {
+            return auctionMapper.getAuctionBidLogByBidId(Integer.parseInt(bid_id));
+        }
+    }
+
+    public List<AuctionBidLog> getAuctionBidLogByAuctionId(int auction_id) {
+        return auctionMapper.getAuctionBidLogByAuctionId(auction_id);
+    }
+
+    public void deleteAuctionBidLog(int bid_id) {
+        auctionMapper.deleteAuctionLogByBidId(bid_id);
     }
 }
